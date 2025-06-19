62c656bd80883d1dad4e10f5d8c87ea9dd75573b
Ilya Ivanov <ilyaivanov@MacBook-Air-Ilya.local>
2025-06-19 17:23:11 +0700

    FinishQuest

---
+++ Assets/Scripts/Systems/TimeMechanismTrigger.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using KingdomOfLinux.Interfaces;
+using KingdomOfLinux.Controllers;
+
+public class TimeMechanismTrigger : MonoBehaviour, IInteractable
+{
+    [TextArea] public string expectedCommand = "sudo timedatectl time-zone KingdomOfLinux";
+
+    public void Interact()
+    {
+        if (PauseController.IsGamePaused) return;
+
+        TerminalController.Instance.OpenTerminal(expectedCommand, () =>
+        {
+            Debug.Log("[TimeMechanismTrigger] Command successful!");
+            TimeMechanismController.Instance.ShowWinnerPanel();
+        });
+
+        FindObjectOfType<InteractionDetector>()?.HideIcon();
+    }
+
+    public bool CanInteract() => !PauseController.IsGamePaused;
+}
